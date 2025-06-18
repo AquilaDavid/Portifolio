d5cd83bed12c716553d4904468e6a76f12860274
AquilaDavid <aquiladavid07@gmail.com>
2025-06-18 13:27:42 -0300

    update de estrutura e arquivo

--- DriveGo/frontend/drivego-front/src/main.jsx
+++ DriveGo/frontend/drivego-front/src/main.jsx
@@ -3,9 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Bem vindo a sua aplicação </div>
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
