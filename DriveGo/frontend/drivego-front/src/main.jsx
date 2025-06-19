8af9282aafd59bc09f5d54e4a459b15d699b2764
AquilaDavid <aquiladavid07@gmail.com>
2025-06-19 03:54:46 -0300

    Atualizações de rotas de navegação

--- DriveGo/frontend/drivego-front/src/main.jsx
+++ DriveGo/frontend/drivego-front/src/main.jsx
@@ -1,18 +1,25 @@
-import { StrictMode } from 'react'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Layout from './layouts/Layout.jsx';
+
 
 const router = createBrowserRouter([
-  {
+ {
     path: '/',
-    element: <div>Bem vindo a sua aplicação </div>
-  },
+    element: <Layout />, // <== layout com o Header + Outlet
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'Home', element: <Home /> },
+      { path: 'Login', element: <Login /> },
+    ]
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
