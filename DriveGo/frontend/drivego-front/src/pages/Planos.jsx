d1c65d67d823b61615c910d13cf13c1021d5f07d
AquilaDavid <aquiladavid07@gmail.com>
2025-06-20 18:29:24 -0300

    Drop de Components do Projeto

--- DriveGo/frontend/drivego-front/src/pages/Planos.jsx
+++
@@ -1,7 +0,0 @@
-export default function Planos() {
-    return(
-        <>
-        <h1>Bem vindo a area de planos</h1>
-        </>
-    )
-}
--- DriveGo/frontend/drivego-front/src/pages/Servicos.jsx
+++
@@ -1,7 +0,0 @@
-export default function Servicos() {
-    return(
-        <>
-        <h1>Bem vindo a area de serviços</h1>
-        </>
-    )
-}
