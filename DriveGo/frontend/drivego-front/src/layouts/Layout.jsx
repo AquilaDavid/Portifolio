6f51556a670cde81089505963205b899f594ad43
AquilaDavid <aquiladavid07@gmail.com>
2025-06-22 00:03:18 -0300

    Update no Layout

--- DriveGo/frontend/drivego-front/src/layouts/Layout.jsx
+++ DriveGo/frontend/drivego-front/src/layouts/Layout.jsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer.jsx";
-
+import ScrollToHeaderButton from "../components/botaoNav/BotaoHeader.jsx";
+import "./Layout.css"; // 👈 importa o CSS
 
 export default function Layout() {
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      <main>
+      <main className="layout-main">
         <Outlet />
+        <ScrollToHeaderButton />
       </main>
-      <Footer/>
-    </>
+      <Footer />
+    </div>
   );
 }
