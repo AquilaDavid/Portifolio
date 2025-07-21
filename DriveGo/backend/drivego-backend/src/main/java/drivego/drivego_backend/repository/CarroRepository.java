c21fe39e6184675743e8b571d1e1da94cccd5075
AquilaDavid <aquiladavid07@gmail.com>
2025-07-20 23:49:19 -0300

    Delete files Java, pom.xml

--- DriveGo/backend/drivego-backend/src/main/java/drivego/drivego_backend/repository/CarroRepository.java
+++
@@ -1,8 +0,0 @@
-package drivego.drivego_backend.repository;
-
-import drivego.drivego_backend.model.Carros;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarroRepository extends JpaRepository <Carros, Long> {
-    
-}
